4fcce1b0118186dccd3fd894359dec95465657b0
PatelArpitt <patelarpit190@gmail.com>
2023-10-13 20:39:07 -0400

    Added: CRUD operations for all the microservices

--- user_service/app.py
+++ user_service/app.py
@@ -1,6 +1,6 @@
 # user_service.py
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -9,17 +9,35 @@ users = {
     '2': {'name': 'Bob', 'email': 'bob@example.com'}
 }
 
-@app.route('/user/<id>')
-def user(id):
+@app.route('/user/<id>', methods = ['GET', 'PATCH', 'DELETE'])
+def userid(id):
+    if request.method == 'GET':
+        user_info = users.get(id, {})
+        return jsonify(user_info)
+    
+    if request.method == 'PATCH':
+        name = request.form['name']
+        email = request.form['email']
 
-    user_info = users.get(id, {})
-    return jsonify(user_info)
+        users[id] = {'name': name, 'email': email}
+        return jsonify(f'User {id} Updated', users[id])
+    
+    if request.method == 'DELETE':
+        del users[id]
+        return jsonify(f'User {id} Deleted', users)
 
-@app.route('/user/<id>')
-def user(id):
+@app.route('/user', methods = ['GET','POST'])
+def user():
+    if request.method == 'GET':
+        return jsonify(users)
+    
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        new_index = str(len(users) + 1)
 
-    user_info = users.get(id, {})
-    return jsonify(user_info)
+        users[new_index] = {'name': name, 'email': email}
+        return jsonify('New User Created', users)
 
 if __name__ == '__main__':
     app.run(port=5000)
